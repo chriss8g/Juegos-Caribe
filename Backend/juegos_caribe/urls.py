7e0c3e1c280cce312a1c61758653621172e72001
Christopher Guerra Herrero <chriss.8.g@citmatel.inf.cu>
2024-02-11 00:27:39 -0500

    feat: comisioner, fix: url season

--- Backend/juegos_caribe/urls.py
+++ Backend/juegos_caribe/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('season.urls'))
+    path('api/season', include('season.urls')),
+    path('api/comisioner', include('comisioner.urls')),
 ]
