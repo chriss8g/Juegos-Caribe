622918dcdda10f420d5672258dc7192b63b33680
Christopher Guerra Herrero <chriss.8.g@citmatel.inf.cu>
2024-02-20 15:46:00 -0500

    fix trash

--- Backend/main/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from django.views.generic import ListView
-from .models import Document
-
-#Cada clase setea la vista de los modelos
-class DocumentView(ListView):
-    model = Document
-    template_name = 'Document.html' #Reemplazar por nombre del html del equipo de Frontend
