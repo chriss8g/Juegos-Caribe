fc75a2d439e3e878dd043881b1aa1ff9b8aaeefe
Christopher Guerra Herrero <chriss.8.g@citmatel.inf.cu>
2024-02-20 16:54:44 -0500

    fix relation season comisioner

--- Backend/cruds/models/season.py
+++ Backend/cruds/models/season.py
@@ -1,9 +1,11 @@
 from django.db import models
+from .comisioner import Comisioner
 
 class Season(models.Model):
     title = models.CharField(max_length=200)
     year = models.IntegerField()
     edition = models.CharField(max_length=10)
+    comisioners = models.ManyToManyField(Comisioner)
 
     def __str__(self):
         return self.title
