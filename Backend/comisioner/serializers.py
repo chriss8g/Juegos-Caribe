2e6da10ca2df0e4cbf4c5927f06f716391c4d099
frankperez-github <fp848584@gmail.com>
2024-02-20 16:14:05 -0500

    not BE on this branch

--- Backend/comisioner/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Comisioner
-    
-class ComisionerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Comisioner
-        fields = ('id', 'name', 'position', 'biography', 'picture')
--- Backend/comisioner/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ListComisioner, DetailComisioner
-
-urlpatterns = [
-    path('<int:pk>/', DetailComisioner.as_view()),
-    path('', ListComisioner.as_view()),
-]
