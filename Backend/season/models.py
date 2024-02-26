95290a3fd9eb29e25be4093017f20bb8ee2d6761
Christopher Guerra Herrero <chriss.8.g@citmatel.inf.cu>
2024-02-26 12:53:51 -0500

    delete trash

--- Backend/season/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Season(models.Model):
-    title = models.CharField(max_length=200)
-    year = models.IntegerField()
-    edition = models.CharField(max_length=10)
-
-    def __str__(self):
-        return self.title
