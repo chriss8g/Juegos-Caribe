c6c58b85e4194fe99692c60d943a63cb8318f31d
Christopher Guerra Herrero <chriss.8.g@citmatel.inf.cu>
2024-03-03 17:24:57 -0500

    fix: body request and add str

--- Backend/cruds/models/tournament.py
+++ Backend/cruds/models/tournament.py
@@ -1,9 +1,7 @@
 from django.db import models
-from .season import Season
 
 class Tournament(models.Model):
     name = models.CharField(max_length=200)
-    season = models.ManyToManyField(Season)
 
     def __str__(self):
         return self.name
