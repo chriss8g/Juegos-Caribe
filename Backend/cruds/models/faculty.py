7d943731b0b3532eeee3baee03c689f69f867359
Christopher Guerra Herrero <chriss.8.g@citmatel.inf.cu>
2024-03-03 23:10:15 -0500

    fix picture format

--- Backend/cruds/models/faculty.py
+++ Backend/cruds/models/faculty.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Faculty(models.Model):
     name = models.CharField(max_length=200)
-    logo = models.FileField()
+    logo = models.ImageField(upload_to='news_pictures/')
 
     def __str__(self):
         return self.name
