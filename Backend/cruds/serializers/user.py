44a671a6d306706f7d7472b1f060fe21144a8306
Marian <mariansusanaas@gmail.com>
2024-03-05 11:30:20 -0500

    str to Users

--- Backend/cruds/serializers/user.py
+++ Backend/cruds/serializers/user.py
@@ -2,6 +2,9 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 class UserSerializer (serializers.ModelSerializer):
+    str = serializers.SerializerMethodField()
+        def get_str(self, obj):
+            return str(obj)
     class Meta:
         model = get_user_model()
         fields = '__all__'
