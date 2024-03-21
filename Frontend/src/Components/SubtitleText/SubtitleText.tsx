8c7fe5772feda5d06c928ca62feadc4bd7a5f838
Amanda <amandacordero.1709@gmail.com>
2024-03-21 01:13:09 -0400

    fix: color and margin

--- Frontend/src/Components/SubtitleText/SubtitleText.tsx
+++ Frontend/src/Components/SubtitleText/SubtitleText.tsx
@@ -7,7 +7,7 @@ export default function SubtitleText({title, text})
             <div className="text-center text-gray-600 pb-3" >
                 <h3 className="font-bold text-[#5a1024ac] mt-6">{title}</h3>
             </div>
-            <p className={`text-gray-400 mt-2`}>{text}</p>
+            <p className={`text mt-2`}>{text}</p>
         </div>
         
     )
