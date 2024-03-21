0847b3279deb93bd72da1ef0d74fcef0ee67ba26
Marian <mariansusanaas@gmail.com>
2024-03-20 23:37:53 -0400

    Fixing merge issues

--- Frontend/src/Components/Layout/Layout.jsx
+++ Frontend/src/Components/Layout/Layout.jsx
@@ -1,16 +1,20 @@
 'use client'
-import {useState} from "react"
 import "./layout.css"
 import "../../app/globals.css"
 import Image from 'next/image'
 import BreadCrumbs from "../BreadCrumbs/BreadCrumbs"
 import Footer from "../Footer/Footer"
-import logo from '../../../public/logo.svg';
-import NavBar from "../NavBar/NavBar";
+import { IconMenu2, IconSearch, IconUser } from "@tabler/icons-react"
+import useAuthentication from "../../hooks/useAuthentication"
+import Link from "next/link"
 
-export default function Layout({children}) {
-    const [lenguage, setLenguage] = useState("es")
-    return (
+export default function Layout({children})
+{
+    const { loggedInUser } = useAuthentication()
+
+
+    // const [lenguage, setLenguage] = useState("es")
+    return(
         <div className="Header">
             <div className="MobileView">
                 <div className="MobileHeader">
@@ -19,30 +23,42 @@ export default function Layout({children}) {
                             <Image alt="" src="/logo.svg" fill className="image"/>
                         </div>
                     </div>
-                    <div className="rightHeader">
-                        <div className="searchIcon">
-                            <Image alt="" src="/search.svg" fill className="image"/>
+                    <div className="rightHeader gap-2 mr-2">
+                        <div className="IconSearch cursor-pointer">
+                            <IconSearch />
                         </div>
-                        <h2 onClick={() => {
-                            lenguage === "es" ? setLenguage("en") : setLenguage("es")
-                        }}>
+                        <Link 
+                            className="LoginIcon cursor-pointer"
+                            href="/LogIn"
+                        >
+                            {`
+                                !loggedInUser.isLoggedIn ?
+                                    <IconUser />
+                                    :
+                                    ${loggedInUser.name[0]?.toUpperCase()+loggedInUser.lastname[0]?.toUpperCase()}
+                            `}
+                        </Link>
+                        {/* <h2 
+                            className="cursor-pointer"
+                            onClick={
+                                ()=>{ lenguage === "es" ? setLenguage("en"): setLenguage("es")}
+                            }>
                             {lenguage.toUpperCase()}
-                        </h2>
+                        </h2> */}
                         <div className="burgerMenu cursor-pointer">
                             <IconMenu2 />
                         </div>
                     </div>
                 </div>
-
             </div>
             <div className="DesktopView">
-                <NavBar/>
+
             </div>
-            <div className="MobileView DesktopView">
-                <BreadCrumbs/>
+            <div className="MobileView">
+                <BreadCrumbs />
             </div>
             {children}
-            <Footer/>
+            <Footer />
         </div>
     )
 }
