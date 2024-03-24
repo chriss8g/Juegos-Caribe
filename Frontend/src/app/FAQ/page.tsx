9d6a6a6f78bf34b4758fabb4d80c673c88dab2e9
CarlosBreso <carlosbresosotto@gmail.com>
2024-03-24 15:09:07 -0400

    FAQs using old TextTittle component

--- Frontend/src/app/FAQ/page.tsx
+++ Frontend/src/app/FAQ/page.tsx
@@ -2,32 +2,26 @@
 import Layout from '../../Components/Layout/Layout';
 import "../../app/globals.css"
 import "./FAQ.css";
-
+import TextWithTitle from '../../Components/TextWithTitle/TextWithTitle';
+import TextWithTitle2 from '../../Components/TextWithTitle2/TextWithTitle2';
 export default function FAQ()
 {    
     return(
         <div className="FAQ">
             <Layout>
-                <div className="FAQ-main-container MobileView" >
+                <div className="FAQ-main-container MobileView container" >
                     
-                    <p className='text-3xl text-gray-600 font-bold mb-5 mt-[5%] faq-header' >Preguntas frecuentes</p>
+                    <TextWithTitle title={"Preguntas frecuentes"} text={""}/>
 
-                    <p className='text-header mb-5 mt-6'>¿Qué son los Juegos Caribe?</p>
+                    <TextWithTitle2 title={"¿Qué son los Juegos Caribe?"} text={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ullam soluta explicabo, illo mollitia corporis, assumenda non tenetur perspiciatis alias dolor velit nihil consectetur vero veniam dignissimos? Corrupti, neque magnam?"}/> 
 
-                    <p className='text-raw mb-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ullam soluta explicabo, illo mollitia corporis, assumenda non tenetur perspiciatis alias dolor velit nihil consectetur vero veniam dignissimos? Corrupti, neque magnam?</p>
+                    <TextWithTitle2 title={"¿Cómo surgieron los Juegos Caribe?"} text={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ullam soluta explicabo, illo mollitia corporis, assumenda non tenetur perspiciatis alias dolor velit nihil consectetur vero veniam dignissimos? Corrupti, neque magnam?"}/>
 
-                    <p className='text-header mb-5'>¿Cómo surgieron los Juegos Caribe?</p>
-
-                    <p className='text-raw mb-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ullam soluta explicabo, illo mollitia corporis, assumenda non tenetur perspiciatis alias dolor velit nihil consectetur vero veniam dignissimos? Corrupti, neque magnam?</p>
-
-                    <p className='text-raw mb-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ullam soluta explicabo, illo mollitia corporis, assumenda non tenetur perspiciatis alias dolor velit nihil consectetur vero veniam dignissimos? Corrupti, neque magnam?</p>
-
-                    <p className='text-raw mb-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ullam soluta explicabo, illo mollitia corporis, assumenda non tenetur perspiciatis alias dolor velit nihil consectetur vero veniam dignissimos? Corrupti, neque magnam?</p>
-
-                    <p className='text-header mb-5'>¿Cuál es el lema de los Juegos Caribe?</p>
+                    <TextWithTitle2 title={""} text={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ullam soluta explicabo, illo mollitia corporis, assumenda non tenetur perspiciatis alias dolor velit nihil consectetur vero veniam dignissimos? Corrupti, neque magnam?"}/>
                     
-                    <p className='text-raw mb-20'>¿Quién vive?... ¡Caribe!... ¿Quién Va?...Universidad!</p>
-                    
+                    <TextWithTitle2 title={""} text={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ullam soluta explicabo, illo mollitia corporis, assumenda non tenetur perspiciatis alias dolor velit nihil consectetur vero veniam dignissimos? Corrupti, neque magnam?"}/>
+
+                    <TextWithTitle2 title={"¿Cuál es el lema de los Juegos Caribe?"} text={"¿Quién vive?... ¡Caribe!... ¿Quién Va?...Universidad!"}/>
                 </div>
             </Layout>
         </div>
