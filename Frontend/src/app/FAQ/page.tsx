8f75db7b4f658fe9470c28d93e5edd68d68c47c6
CarlosBreso <carlosbresosotto@gmail.com>
2024-03-24 15:27:07 -0400

    FAQs con componentes actualizadas. Modifiqué el texto TextTitle añadiendole "text-start"

--- Frontend/src/app/FAQ/page.tsx
+++ Frontend/src/app/FAQ/page.tsx
@@ -2,8 +2,8 @@
 import Layout from '../../Components/Layout/Layout';
 import "../../app/globals.css"
 import "./FAQ.css";
-import TextWithTitle from '../../Components/TextWithTitle/TextWithTitle';
-import TextWithTitle2 from '../../Components/TextWithTitle2/TextWithTitle2';
+import TitleText from '../../Components/TitleText/TitleText';
+import SubtitleText from '../../Components/SubtitleText/SubtitleText';
 export default function FAQ()
 {    
     return(
@@ -11,17 +11,17 @@ export default function FAQ()
             <Layout>
                 <div className="FAQ-main-container MobileView container" >
                     
-                    <TextWithTitle title={"Preguntas frecuentes"} text={""}/>
+                    <TitleText title={"Preguntas frecuentes"} text={""}/>
 
-                    <TextWithTitle2 title={"¿Qué son los Juegos Caribe?"} text={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ullam soluta explicabo, illo mollitia corporis, assumenda non tenetur perspiciatis alias dolor velit nihil consectetur vero veniam dignissimos? Corrupti, neque magnam?"}/> 
+                    <SubtitleText title={"¿Qué son los Juegos Caribe?"} text={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ullam soluta explicabo, illo mollitia corporis, assumenda non tenetur perspiciatis alias dolor velit nihil consectetur vero veniam dignissimos? Corrupti, neque magnam?"}/> 
 
-                    <TextWithTitle2 title={"¿Cómo surgieron los Juegos Caribe?"} text={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ullam soluta explicabo, illo mollitia corporis, assumenda non tenetur perspiciatis alias dolor velit nihil consectetur vero veniam dignissimos? Corrupti, neque magnam?"}/>
+                    <SubtitleText title={"¿Cómo surgieron los Juegos Caribe?"} text={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ullam soluta explicabo, illo mollitia corporis, assumenda non tenetur perspiciatis alias dolor velit nihil consectetur vero veniam dignissimos? Corrupti, neque magnam?"}/>
 
-                    <TextWithTitle2 title={""} text={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ullam soluta explicabo, illo mollitia corporis, assumenda non tenetur perspiciatis alias dolor velit nihil consectetur vero veniam dignissimos? Corrupti, neque magnam?"}/>
+                    <SubtitleText title={""} text={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ullam soluta explicabo, illo mollitia corporis, assumenda non tenetur perspiciatis alias dolor velit nihil consectetur vero veniam dignissimos? Corrupti, neque magnam?"}/>
                     
-                    <TextWithTitle2 title={""} text={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ullam soluta explicabo, illo mollitia corporis, assumenda non tenetur perspiciatis alias dolor velit nihil consectetur vero veniam dignissimos? Corrupti, neque magnam?"}/>
+                    <SubtitleText title={""} text={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ullam soluta explicabo, illo mollitia corporis, assumenda non tenetur perspiciatis alias dolor velit nihil consectetur vero veniam dignissimos? Corrupti, neque magnam?"}/>
 
-                    <TextWithTitle2 title={"¿Cuál es el lema de los Juegos Caribe?"} text={"¿Quién vive?... ¡Caribe!... ¿Quién Va?...Universidad!"}/>
+                    <SubtitleText title={"¿Cuál es el lema de los Juegos Caribe?"} text={"¿Quién vive?... ¡Caribe!... ¿Quién Va?...Universidad!"}/>
                 </div>
             </Layout>
         </div>
