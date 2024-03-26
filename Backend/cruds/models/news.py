b3044995e69f37e765e889bf765c70e5c4899cca
Christopher Guerra Herrero <chriss.8.g@citmatel.inf.cu>
2024-03-26 16:21:21 -0400

    fix media

--- Backend/cruds/models/news.py
+++ Backend/cruds/models/news.py
@@ -5,7 +5,7 @@ class News (models.Model):
     title = models.CharField(max_length=200)
     body = models.CharField(max_length=1000)
     date = models.DateTimeField(auto_now_add=True)
-    picture = models.ImageField(upload_to='news_pictures/')
+    picture = models.ImageField(upload_to='')
     user = models.ForeignKey(get_user_model(), on_delete=models.PROTECT )
 
     def __str__(self):
--- Backend/juegos_caribe/settings.py
+++ Backend/juegos_caribe/settings.py
@@ -160,3 +160,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/new_files/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'new_files')
