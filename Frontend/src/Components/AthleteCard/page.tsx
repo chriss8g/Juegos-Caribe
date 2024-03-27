a32a609c6cb8f7d6784a6ff77451172758926414
frankperez-github <fp848584@gmail.com>
2024-03-27 17:24:48 -0400

    feat/connect with BE

--- Frontend/src/Components/AthleteCard/page.tsx
+++ Frontend/src/Components/AthleteCard/page.tsx
@@ -1,16 +1,20 @@
+import { useEffect } from "react"
+
 export default function AthleteCard({athlete})
 {
-
+    useEffect(()=>{
+        console.log("atleta: ",athlete)
+    },[])
 
     return(
         <div className="athlete-card-main-container flex w-[90%] m-2 bg-white py-3 px-4 rounded-xl shadow-lg items-center">
             <div className="w-[35%] mr-1">
-                <img src={athlete[0].imagen} alt="jugador" className=""/>
+                <img src={athlete.picture} alt="jugador" className=""/>
             </div>
             <div className="flex flex-col">
-                <p className="font-medium text-sm">{athlete[0].num} {athlete[0].name}</p>
-                <p className="">{athlete[0].sport}</p>
-                <p className="">{athlete[0].pos}</p>
+                <p className="font-medium text-sm">{athlete.name}</p>
+                {/* <p className="">{athlete.sport}</p> */}
+                {/* <p className="">{athlete.pos}</p> */}
             </div>
         </div>
     )
--- Frontend/src/app/Facultad/[facultyId]/page.tsx
+++ Frontend/src/app/Facultad/[facultyId]/page.tsx
@@ -10,61 +10,31 @@ import MedalsFac from "../../../Components/MedalsFac/page";
 import useAdministration from "../../../hooks/useAdministration";
 import axios from "axios";
 import { useParams } from "next/navigation";
+import useEntityInformation from "../../../hooks/useEntityInformation";
 
 
 export default function FacultadDetallada()
 {
+    const{getData, getDataByIdFromEndpoint, DataByIdFromEndpoint, Data} = useAdministration()
     const{facultyId}= useParams();
 
-    const CFacultad =[
-        {
-            "id": 1,
-            "str": "INSTEC",
-            "name": "Instituto de Ciencias y Tecnologías Aplicadas.",
-            "logo": "/instec.svg"
-        }]
-
-    const CAthlete =[
-        {
-            "id": 1,
-            "sport": "Fútbol",
-            "name": "Luka Modric Ramírez",
-            "imagen": "/athlete1.png",
-            "pos": "Medio Campo",
-            "num": 10
-        },
-        {
-            "id":2,
-            "sport": "Fútbol",
-            "name": "Luka Modric Ramírez",
-            "imagen": "/athlete1.png",
-            "pos": "Medio Campo",
-            "num": 10
-        },
-        {
-            "id":3,
-            "sport": "Fútbol",
-            "name": "Luka Modric Ramírez",
-            "imagen": "/athlete1.png",
-            "pos": "Medio Campo",
-            "num": 10
-        }]
-
-    const{getData, getDataByIdFromEndpoint, DataByIdFromEndpoint, Data} = useAdministration()
 
     useEffect(()=>{
         getDataByIdFromEndpoint(+facultyId, "/faculty")
         getData(`${process.env.API_URL}/faculty/${facultyId}/athletes`)
     },[])
 
-    const[faculty, setFaculty] = useState({});
-    const[athletes, setAthletes] = useState({});
-    
+
+    const[faculty, setFaculty] = useState({} as Faculty);
+    const[athletes, setAthletes] = useState([]);
+
     useEffect(()=>{
-        setFaculty(DataByIdFromEndpoint);
-        setAthletes(Data);
-        console.log(Data)
+        setFaculty(DataByIdFromEndpoint as Faculty);
     },[DataByIdFromEndpoint]);
+
+    useEffect(()=>{
+        setAthletes(Data);
+    },[Data]);
 
 
     const [category, setCategory] = useState("");
@@ -72,36 +42,39 @@ export default function FacultadDetallada()
     return (
         <div className="fac-det-main-container MobileView">
             <Layout>
-                <div className="flex flex-col items-center justify-center">
-                    <img src={CFacultad[0].logo} alt="logo de la facultad" className="w-1/2" />
-                    <p className="text-gray-500 font-bold text-2xl px-3 text-center">{CFacultad[0].name}</p>
-                    <p className="text-red-950 font-medium text-lg mt-7">Atletas destacados</p>
+                {
+                    faculty && athletes.length > 0 &&
+                        <div className="flex flex-col items-center justify-center">
+                            <img src={faculty.logo} alt="logo de la facultad" className="w-1/2" />
+                            <p className="text-gray-500 font-bold text-2xl px-3 text-center">{faculty.name}</p>
+                            <p className="text-red-950 font-medium text-lg mt-7">Atletas destacados</p>
 
-                    {CAthlete.map((athlete) => (
-                        <AthleteCard
-                            key={athlete.id}
-                            athlete={CAthlete}
-                        />
-                    ))}
+                            {athletes.map((athlete, id) => (
+                                <AthleteCard
+                                    key={id}
+                                    athlete={athlete}
+                                />
+                            ))}
 
-                    <p className="text-red-950 font-medium text-lg mt-7">Deportes que participan:</p>
-                    <div className="part-sports-main-container flex justify-between text-red-950 w-5/6 mt-7 border-b-[2px] border-b-red-950">
-                            <p className={`w-1/2 text-lg mx-auto text-center pb-1 ${category === " Colectivos" ? " text-gray-500" : " sport-category  text-red-950"}`} onClick={() => setCategory(" Individuales")}>Individuales</p>
-                            <p className={`w-1/2 text-lg mx-auto text-center pb-1 ${category === " Colectivos" ? " text-red-950 sport-category " : " text-gray-500"}`} onClick={() => setCategory(" Colectivos")}>Colectivos</p>
-                     </div>
-                    <LessMoreBar title={"Ajedrez"} text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur minus excepturi dicta veniam inventore rem voluptas neque magni nulla. Quasi sed reprehenderit quo architecto quaerat illo possimus tenetur culpa nesciunt!"/>
-                    <LessMoreBar title={"Baloncesto"} text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur minus excepturi dicta veniam inventore rem voluptas neque magni nulla. Quasi sed reprehenderit quo architecto quaerat illo possimus tenetur culpa nesciunt!"/>
-                    <LessMoreBar title={"Baloncesto 3x3"} text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur minus excepturi dicta veniam inventore rem voluptas neque magni nulla. Quasi sed reprehenderit quo architecto quaerat illo possimus tenetur culpa nesciunt!"/>
-                    <LessMoreBar title={"Balonmano"} text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur minus excepturi dicta veniam inventore rem voluptas neque magni nulla. Quasi sed reprehenderit quo architecto quaerat illo possimus tenetur culpa nesciunt!"/>
+                            <p className="text-red-950 font-medium text-lg mt-7">Deportes que participan:</p>
+                            <div className="part-sports-main-container flex justify-between text-red-950 w-5/6 mt-7 border-b-[2px] border-b-red-950">
+                                    <p className={`w-1/2 text-lg mx-auto text-center pb-1 ${category === " Colectivos" ? " text-gray-500" : " sport-category  text-red-950"}`} onClick={() => setCategory(" Individuales")}>Individuales</p>
+                                    <p className={`w-1/2 text-lg mx-auto text-center pb-1 ${category === " Colectivos" ? " text-red-950 sport-category " : " text-gray-500"}`} onClick={() => setCategory(" Colectivos")}>Colectivos</p>
+                            </div>
+                            <LessMoreBar title={"Ajedrez"} text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur minus excepturi dicta veniam inventore rem voluptas neque magni nulla. Quasi sed reprehenderit quo architecto quaerat illo possimus tenetur culpa nesciunt!"/>
+                            <LessMoreBar title={"Baloncesto"} text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur minus excepturi dicta veniam inventore rem voluptas neque magni nulla. Quasi sed reprehenderit quo architecto quaerat illo possimus tenetur culpa nesciunt!"/>
+                            <LessMoreBar title={"Baloncesto 3x3"} text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur minus excepturi dicta veniam inventore rem voluptas neque magni nulla. Quasi sed reprehenderit quo architecto quaerat illo possimus tenetur culpa nesciunt!"/>
+                            <LessMoreBar title={"Balonmano"} text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur minus excepturi dicta veniam inventore rem voluptas neque magni nulla. Quasi sed reprehenderit quo architecto quaerat illo possimus tenetur culpa nesciunt!"/>
 
 
 
-                    <p className="text-black font-medium text-lg mt-7">Medallero</p>
-                    <MedalsFac/>
+                            <p className="text-black font-medium text-lg mt-7">Medallero</p>
+                            <MedalsFac/>
 
-                    <p className="text-black font-medium text-lg mt-7">Resultados</p>
-                    <Results data="" />
-                </div>
+                            <p className="text-black font-medium text-lg mt-7">Resultados</p>
+                            <Results data="" />
+                        </div>
+                }
             </Layout>
         </div>
     );
