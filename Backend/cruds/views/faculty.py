b1295554973f7bc657f768d714b69c2327ea1060
Christopher Guerra Herrero <chriss.8.g@citmatel.inf.cu>
2024-03-28 11:01:55 -0400

    feat faculty with medals

--- Backend/cruds/views/faculty.py
+++ Backend/cruds/views/faculty.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse
 
 from ..models.faculty import Faculty
 from ..models.athlete import Athlete
+from ..models.team import Team
 
 class FacultyList(generics.ListCreateAPIView):
     queryset = Faculty.objects.all()
@@ -13,7 +14,29 @@ class FacultyDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Faculty.objects.all()
     serializer_class = FacultySerializer
 
-def Athletes_per_faculty(request, id_faculty):
+def athletesPerFaculty(request, id_faculty):
     athlete = Athlete.objects.filter(faculty = id_faculty)
     athlete_data = list(athlete.values())
     return JsonResponse(athlete_data, safe=False)
+
+def facultiesWithMedals(request):
+    faculties = Faculty.objects.all()
+    data = []
+    
+    for faculty in faculties:
+        medals_faculty = {}
+        medals_faculty['faculty'] = faculty.name
+        medals_faculty['logo'] =  faculty.logo.url if faculty.logo else None
+        medals_faculty['medals'] = {}
+        
+        teams_faculty = Team.objects.filter(faculty=faculty)
+        for team in teams_faculty:
+            medal_type = team.medal.data
+            if medal_type in medals_faculty['medals']:
+                medals_faculty['medals'][medal_type] += 1
+            else:
+                medals_faculty['medals'][medal_type] = 1
+                
+        data.append(medals_faculty)
+        
+    return JsonResponse(data, safe=False)
