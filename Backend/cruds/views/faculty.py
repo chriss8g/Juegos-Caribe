272f331daca4ac07a9872a0efdff93c77176220c
Christopher Guerra Herrero <chriss.8.g@citmatel.inf.cu>
2024-03-29 09:00:59 -0400

    fix picture in faculty with medals

--- Backend/cruds/views/faculty.py
+++ Backend/cruds/views/faculty.py
@@ -26,17 +26,24 @@ def facultiesWithMedals(request):
     for faculty in faculties:
         medals_faculty = {}
         medals_faculty['faculty'] = faculty.name
-        medals_faculty['logo'] =  faculty.logo.url if faculty.logo else None
+        logo_url = faculty.logo.url if faculty.logo else None
+        medals_faculty['logo'] = request.build_absolute_uri(logo_url) if logo_url else None
         medals_faculty['medals'] = {}
+        
+        total = 0
         
         teams_faculty = Team.objects.filter(faculty=faculty)
         for team in teams_faculty:
             medal_type = team.medal.data
-            if medal_type in medals_faculty['medals']:
-                medals_faculty['medals'][medal_type] += 1
-            else:
-                medals_faculty['medals'][medal_type] = 1
-                
+
+            if(medal_type):
+                total = total + 1
+                if medal_type in medals_faculty['medals']:
+                    medals_faculty['medals'][medal_type] += 1
+                else:
+                    medals_faculty['medals'][medal_type] = 1
+        
+        medals_faculty['total'] = total
         data.append(medals_faculty)
         
     return JsonResponse(data, safe=False)
