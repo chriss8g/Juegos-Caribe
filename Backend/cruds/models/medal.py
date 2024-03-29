1cc942dd335c1959ee156140f943f4aaedff40c9
frankperez-github <fp848584@gmail.com>
2024-03-29 14:18:34 -0400

    medal type

--- Backend/cruds/models/medal.py
+++ Backend/cruds/models/medal.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Medal(models.Model):
 
-    data = models.CharField(max_length=200)
+    type = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.data
+        return self.type
