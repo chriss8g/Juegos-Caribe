4850315bb0ccaee7d2f032a38bafa14f01f1c872
Christopher Guerra Herrero <chriss.8.g@citmatel.inf.cu>
2024-03-29 16:12:31 -0400

    feat view RecentNews

--- Backend/cruds/models/news.py
+++ Backend/cruds/models/news.py
@@ -3,8 +3,8 @@ from django.contrib.auth import get_user_model
 
 class News (models.Model):
     title = models.CharField(max_length=200)
-    body = models.CharField(max_length=1000)
-    date = models.DateTimeField(auto_now_add=True)
+    body = models.TextField()
+    date = models.DateField(auto_now_add=True)
     picture = models.ImageField(upload_to='')
     user = models.ForeignKey(get_user_model(), on_delete=models.PROTECT )
 
