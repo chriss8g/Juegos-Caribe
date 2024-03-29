06fed06deda6550f0f8f640e63cacbf8b3c2eb2b
Marian <mariansusanaas@gmail.com>
2024-03-29 19:40:16 -0400

    carousel design improvements

---
+++ Frontend/src/app/Facultades/page.tsx
@@ -0,0 +1,16 @@
+import "../../app/globals.css"
+import Layout from "../../Components/Layout/Layout";
+
+export default function Facultades()
+{
+
+    return(
+        <Layout>
+            <div className= "MobileView container">
+                    <div className="">
+
+                    </div>
+            </div>
+        </Layout>
+    )
+}
