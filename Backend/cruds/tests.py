c96ed85d461d8a904a643a30cfab62ebe7d67ddd
CarlosBreso <carlosbresosotto@gmail.com>
2024-03-30 12:11:05 -0400

    Fk usage tested

--- Backend/cruds/tests.py
+++ Backend/cruds/tests.py
@@ -1,23 +1,42 @@
 from django.test import TestCase
 from .models.season import Season
-
+from.models.faculty import Faculty
+from .models.athlete import Athlete
+from .models.document import Document
+from .models.medal import Medal
+from .models.comment import Comment
+from .models.news import News
+from django.contrib.auth import get_user_model
+#date field
+#user
+#authentication
+#ManytoMany
 class SeasonModelTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
         Season.objects.create(title='first Season', year='2023', edition='LV')
-
-    def test_title_content(self):
+        
+        Faculty.objects.create(name='Matcom')
+        
+        Athlete.objects.create(name= 'Estudiante Genérica', biography="Fuí monitora en la primaria" ,faculty=Faculty.objects.get(id=1))
+        
+        #News.objects.create(tittle="No me gustó el arbitraje", body="Candela")
+    
+    def test_Season_content(self):
         season = Season.objects.get(id=1)
-        expected_object_name = f'{season.title}'
-        self.assertEquals(expected_object_name, 'first Season')
+        title = f'{season.title}'
+        self.assertEquals(title, 'first Season')
+        year = f'{season.year}'
+        self.assertEquals(year, '2023')
+        edition = f'{season.edition}'
+        self.assertEquals(edition, 'LV')
 
-    def test_year_content(self):
-        season = Season.objects.get(id=1)
-        expected_object_name = f'{season.year}'
-        self.assertEquals(expected_object_name, '2023')
-
-    def test_edition_content(self):
-        season = Season.objects.get(id=1)
-        expected_object_name = f'{season.edition}'
-        self.assertEquals(expected_object_name, 'LV')
+    def test_Faculty_content(self):
+        athlete= Athlete.objects.get(id=1)
+        faculty = athlete.faculty
+        facName = faculty.name
+        biography = athlete.biography
+        
+        self.assertEqual(facName, 'Matcom')
+        self.assertEqual(biography, 'Fuí monitora en la primaria')
