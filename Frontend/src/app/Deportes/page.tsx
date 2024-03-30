b327ac007c2b5bd8d43f7315e88905db4e624c70
Christopher Guerra Herrero <chriss.8.g@citmatel.inf.cu>
2024-03-30 10:57:57 -0400

    connect Sport

--- Frontend/src/app/Deportes/page.tsx
+++ Frontend/src/app/Deportes/page.tsx
@@ -1,16 +1,35 @@
+'use client'
 import Layout from "../../Components/Layout/Layout";
 import LittleSportCard from "../../Components/LittleSportCard/LittleSportCard";
+import useAdministration from "../../hooks/useAdministration";
+import { useEffect, useState } from "react"
+
 export default function Sports()
 {
+    const{getData, Data} = useAdministration()
+    
+    useEffect(()=>{
+        getData(`${process.env.API_URL}/sport`)
+    },[])
+
+    const[sports, setSports] = useState([]);
+
+    useEffect(()=>{
+        setSports(Data);
+    },[Data]);
+
     return(
+        sports.length > 0 &&
         <Layout>
             <h2 className="text-center text-rose-950 my-10 mb-5 font-bold">Deportes</h2>
             <div className="display flex flex-wrap wrap px-7">
-                <LittleSportCard sportName={"Atletismo"}/>
-                <LittleSportCard sportName={"Taekwondo"}/>
-                <LittleSportCard sportName={"Tenis"}/>
-                <LittleSportCard sportName={"Taekwondo2"}/>
-                <LittleSportCard sportName={"Tenis2"}/>
+            {
+                sports.map((sport, key)=>(
+                    <div key={key}>
+                        <LittleSportCard sportName={sport.name} sportPicture={sport.logo}/>
+                    </div>
+                ))
+            }
             </div>
         </Layout>
     )
