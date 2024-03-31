3b450db0e37ed0825886892d27a46de6417f72e4
frankperez-github <fp848584@gmail.com>
2024-03-31 02:00:12 -0400

    add style to administration

--- Frontend/src/app/Administration/page.tsx
+++ Frontend/src/app/Administration/page.tsx
@@ -149,7 +149,7 @@ export default function Administration()
                                         Data.length > 0 ?
                                             <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
                                                 <table className="w-full text-md text-left rtl:text-right text-black">
-                                                    <thead className=" text-center text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                                    <thead className="text-center text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                                         <tr>
                                                             <th scope="col" className="p-4">
                                                                 <input type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" name="select_all" id={`${Data.map((row, i)=>i)}`} onChange={(e)=>handleSelect(e, true)}/>
