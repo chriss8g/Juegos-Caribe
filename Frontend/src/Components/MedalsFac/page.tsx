3452a645faca0f5263309cf7aa0853460b32657c
Christopher Guerra Herrero <chriss.8.g@citmatel.inf.cu>
2024-03-30 20:03:33 -0400

    feat table detail medal

--- Frontend/src/Components/MedalsFac/page.tsx
+++ Frontend/src/Components/MedalsFac/page.tsx
@@ -1,11 +1,30 @@
 'use client'
 import Image from "next/image"
 import "./medalsFac.css";
-export default function MedalsFac()
+import useAdministration from "../../hooks/useAdministration";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+
+export default function MedalsFac({facultyId})
 {
+    const{getDataByIdFromEndpoint, DataByIdFromEndpoint} = useAdministration()
+
+
+    useEffect(()=>{
+        getDataByIdFromEndpoint(+facultyId, "/faculty/medals")
+    },[])
+
+    const[info, setInfo] = useState<Medals>();
+
+    useEffect(()=>{
+        setInfo(DataByIdFromEndpoint as unknown as Medals);
+    },[DataByIdFromEndpoint]);
 
     return(
+        info &&
         <div className="MobileView">
+            <p className="text-black font-medium text-lg mt-7">Medallero</p>
             <div className="fac-medals-main-container flex justify-center">
                 <table className="medalsTable border-separate border-spacing-y-3">
                     <thead className="fac-table-header">
@@ -39,15 +58,14 @@ export default function MedalsFac()
                             <tr className="trow">
                                 <td>
                                     <div className="row">
-                                        <p>10</p>
+                                        <p>{info["points"]}</p>
                                     </div>
                                 </td>
-                                <td className="medalInfo">1</td>
-                                <td className="medalInfo">-</td>
-                                <td className="medalInfo">4</td>
-                                <td className="medalInfo">5</td>
+                                <td className="medalInfo">{info.medals['Oro']?   info.medals['Oro']: '-'}</td>
+                                <td className="medalInfo">{info.medals['Plata']? info.medals['Plata']: '-'}</td>
+                                <td className="medalInfo">{info.medals['Bronce']?info.medals['Bronce']: '-'}</td>
+                                <td className="medalInfo">{info["total"]}</td>
                             </tr>
-
                         </>
 
                     </tbody>
--- Frontend/src/app/Facultades/[facultyId]/page.tsx
+++ Frontend/src/app/Facultades/[facultyId]/page.tsx
@@ -5,12 +5,10 @@ import { useEffect, useState } from "react";
 import Results from "../../../Components/Results/page";
 import "../fac-detallada.css"
 import LessMoreBar from "../../../Components/Less-More-Bar/LessMoreBar";
-import MedalTable from "../../../Components/MedalTable/MedalTable";
 import MedalsFac from "../../../Components/MedalsFac/page";
 import useAdministration from "../../../hooks/useAdministration";
-import axios from "axios";
 import { useParams } from "next/navigation";
-import useEntityInformation from "../../../hooks/useEntityInformation";
+import SportsPerFaculty from "../../../Components/SportsPerFaculty/SportsPerFaculty";
 
 
 export default function FacultadDetallada()
@@ -64,20 +62,9 @@ export default function FacultadDetallada()
                                 
                             }
 
-                            <p className="text-red-950 font-medium text-lg mt-7">Deportes que participan:</p>
-                            <div className="part-sports-main-container flex justify-between text-red-950 w-5/6 mt-7 border-b-[2px] border-b-red-950">
-                                    {/* <p className={`w-1/2 text-lg mx-auto text-center pb-1 ${category === " Colectivos" ? " text-gray-500" : " sport-category  text-red-950"}`} onClick={() => setCategory(" Individuales")}>Individuales</p> */}
-                                    <p className={`w-1/2 text-lg mx-auto text-center pb-1 ${category === " Colectivos" ? " text-red-950 sport-category " : " text-gray-500"}`} onClick={() => setCategory(" Colectivos")}>Colectivos</p>
-                            </div>
-                            <LessMoreBar title={"Ajedrez"} text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur minus excepturi dicta veniam inventore rem voluptas neque magni nulla. Quasi sed reprehenderit quo architecto quaerat illo possimus tenetur culpa nesciunt!"/>
-                            <LessMoreBar title={"Baloncesto"} text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur minus excepturi dicta veniam inventore rem voluptas neque magni nulla. Quasi sed reprehenderit quo architecto quaerat illo possimus tenetur culpa nesciunt!"/>
-                            <LessMoreBar title={"Baloncesto 3x3"} text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur minus excepturi dicta veniam inventore rem voluptas neque magni nulla. Quasi sed reprehenderit quo architecto quaerat illo possimus tenetur culpa nesciunt!"/>
-                            <LessMoreBar title={"Balonmano"} text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur minus excepturi dicta veniam inventore rem voluptas neque magni nulla. Quasi sed reprehenderit quo architecto quaerat illo possimus tenetur culpa nesciunt!"/>
-
-
-
-                            <p className="text-black font-medium text-lg mt-7">Medallero</p>
-                            <MedalsFac/>
+                            <SportsPerFaculty facultyId={facultyId}/>
+                        
+                            <MedalsFac facultyId={facultyId}/>
 
                             <p className="text-black font-medium text-lg mt-7">Resultados</p>
                             <Results data="" />
