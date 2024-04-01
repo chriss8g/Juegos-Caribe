4325c56d94790be5adfd4b8de757e9790cd19d69
Christopher Guerra Herrero <chriss.8.g@citmatel.inf.cu>
2024-03-31 23:05:43 -0400

    fix descriptions in commissioner

--- Frontend/src/app/Comisionado/page.jsx
+++ Frontend/src/app/Comisionado/page.jsx
@@ -1,113 +1,62 @@
 'use client'
 import Layout from "../../Components/Layout/Layout";
 import "../../Components/FacultyPreview/FacultyPreview.css"
-import { useEffect, useState } from "react"
+import {useState, useEffect} from "react"
+import useAdministration from "../hooks/useAdministration";
 import Pagination from '../../Components/Pagination/Pagination'
 import "../globals.css"
 import CardITB from "../../Components/CardITB/CardITB";
 import ComisionerCard from "../../Components/ComisionerCard/comisionerCard";
 
-export default function Comisioner()
+export default function Commissioner()
 {
-    const comisioner = [
-        {
-            "name": "Elemento 1",
-            "image": "/comisionado.png",
-            "posicion": "Dir. de la DIrección de Cultura Fisica y Deporte (INDER)",
-            "comision": "organizativa"
-        },
-        {
-            "name": "Elemento 2",
-            "image": "/comisionado.png",
-            "posicion": "Dir. de la Dirección de Cultura Fisica y Deporte (INDER)",
-            "comision": "organizativa"
-        },
-        {
-            "name": "Elemento 3",
-            "image": "/comisionado.png",
-            "posicion": "Dir. de la Dirección de Cultura Fisica y Deporte (INDER)",
-            "comision": "organizativa"
-        },
-        {
-            "name": "Elemento 4",
-            "image": "/comisionado.png",
-            "posicion": "Dir. de la Dirección de Cultura Fisica y Deporte (INDER)",
-            "comision": "organizativa"
-        },
-        {
-            "name": "Elemento 5",
-            "image": "/comisionado.png",
-            "posicion": "Dir. de la Dirección de Cultura Fisica y Deporte (INDER)",
-            "comision": "disciplinariaS"
-        },
-        {
-            "name": "Elemento 6",
-            "image": "/comisionado.png",
-            "posicion": "Dir. de la Dirección de Cultura Fisica y Deporte (INDER)",
-            "comision": "disciplinariaS"
-        },
-        {
-            "name": "Elemento 7",
-            "image": "/comisionado.png",
-            "posicion": "Dir. de la Dirección de Cultura Fisica y Deporte (INDER)",
-            "comision": "disciplinariaS"
-        }
-        ,
-        {
-            "name": "Elemento 8",
-            "image": "/comisionado.png",
-            "posicion": "Dir. de la Dirección de Cultura Fisica y Deporte (INDER)",
-            "comision": "disciplinariaP"
-        },
-        {
-            "name": "Elemento 9",
-            "image": "/comisionado.png",
-            "posicion": "Dir. de la Dirección de Cultura Fisica y Deporte (INDER)",
-            "comision": "disciplinariaP"
-        }
-        ,
-        {
-            "name": "Elemento 10",
-            "image": "/comisionado.png",
-            "posicion": "Dir. de la Dirección de Cultura Fisica y Deporte (INDER)",
-            "comision": "disciplinariaP"
-        }
-    ]
+    const{getData, Data} = useAdministration()
+    
+    useEffect(()=>{
+        getData(`${process.env.API_URL}/faculty/lastSeason`)
+    },[])
 
-        // Paso 1: Dividir la lista comisioner
+    const[commissioner, setCommissioner] = useState([]);
+
+    useEffect(()=>{
+        setCommissioner(Data);
+    },[Data]);
+
+
     const [organizativa, setOrganizativa] = useState([]);
     const [disciplinariaP, setDisciplinariaP] = useState([]);
     const [disciplinariaS, setDisciplinariaS] = useState([]);
 
     useEffect(() => {
-        // Paso 2: Renombrar los campos y dividir la lista
-        const organizativaList = comisioner.filter(item => item.comision === 'organizativa').map(item => ({
+        const organizativaList = commissioner.filter(item => item.comision === 'Comisión Organizativa').map(item => ({
             name: item.name,
             body: item.posicion,
             image: item.image
         }));
 
-        const disciplinariaPList = comisioner.filter(item => item.comision === 'disciplinariaP').map(item => ({
+        const disciplinariaPList = commissioner.filter(item => item.comision === 'Comisión Disciplinaria Permanente').map(item => ({
             name: item.name,
             body: item.posicion,
             image: item.image
         }));
 
-        const disciplinariaSList = comisioner.filter(item => item.comision === 'disciplinariaS').map(item => ({
+        const disciplinariaSList = commissioner.filter(item => item.comision === 'Comisión Disciplinaria Suplente').map(item => ({
             name: item.name,
             body: item.posicion,
             image: item.image
         }));
-        // Paso 3: Actualizar el estado
         setOrganizativa(organizativaList);
         setDisciplinariaP(disciplinariaPList);
         setDisciplinariaS(disciplinariaSList);
     }, []);
 
     return(
-        
+
         <div className="Comisioner MobileView">
             <Layout>
+            {
+                commissioner.length > 0 &&
+                <div>
                 <h2 className="flex justify-center font-bold text-2xl text-gray-800 mt-6">Comisión</h2>
                 <div className="font-text-6">
                     <p className="text-red-950 mt-6 text-center text-lg mb-3">Comisión Organizativa</p>
@@ -145,6 +94,8 @@ export default function Comisioner()
                     </div>
 
                 </div>
+                </div>
+            }
             </Layout>
         </div>
     )
