b86d2603e44ea6ee746550c2b83b3f18bdb11cde
frankperez-github <fp848584@gmail.com>
2024-04-02 22:42:29 -0400

    merge

--- Frontend/src/Components/PlayCard/PlayCard.tsx
+++ Frontend/src/Components/PlayCard/PlayCard.tsx
@@ -6,46 +6,46 @@ import useSiteContext from "../../hooks/useSiteContext"
 export default function PlayCard({play})
 {
     const[isLoading, setIsLoading] = useState(true)
-    const {Faculties}= useSiteContext()
+    const { Faculties }= useSiteContext()
     const [logo1, setLogo1] = useState("")
     const [logo2, setLogo2] = useState("")
 
     useEffect(()=>
     {
+        console.log(play.teams)
         Faculties.map((fac)=>
         {
-            if(play.teams[0] === fac.name)
-            {
-                setLogo1(fac.logo)
-            }
-            if(play.teams[1] === fac.name)
-            {
-                setLogo2(fac.logo)
-            }
+            // if(play.teams[0] === fac.name)
+            // {
+            //     setLogo1(fac.logo)
+            // }
+            // if(play.teams[1] === fac.name)
+            // {
+            //   setLogo2(fac.logo)
+            // }
         })
-    }, [])
+    }, [play])
 
     
 
 
     return(
-        isLoading &&
         <div className="MobileView play-card-container mb-10">
             <div className="MobileView PlayCard">
                 <div className="row">
                     <div className="left column">
-                        {/* <div className="facLogo">
+                        <div className="facLogo">
                             <Image src={logo1} fill className="image" alt=""/>
-                        </div> */}
+                        </div>
                     </div>
                     <div className="center column">
                         <p className="font-bold text-gray-700 w-[100%] pt-[10%] pb-1/5 text-[1rem]">{play.sport} {play.sex === "Masculino" ? "(M)" : play.sex === "Femenino" ? "(F)": "(Mix)"}</p>
                         <p className="font-bold text-xl text-gray-700 pt-[10%]">{play.points[0]}-{play.points[1]}</p>
                     </div>
                     <div className="right column">
-                        {/* <div className="facLogo">
+                        <div className="facLogo">
                             <Image src={logo2} fill className="image" alt=""/>
-                        </div> */}
+                        </div>
                     </div>
                 </div>
                 <div className="row teamsRow">
@@ -64,6 +64,5 @@ export default function PlayCard({play})
                 </div> */}
                 <p className="text-gray-700 text-sm">{play.detailResult}</p>
         </div> 
-        
     )
 }
